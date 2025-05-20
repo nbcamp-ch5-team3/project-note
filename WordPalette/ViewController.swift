e4b4fb4554af6c0df6f437890cd448a113807c74
gnoes-ios <park_juseong@naver.com>
2025-05-20 14:44:10 +0900

    Initial Commit

---
+++ WordPalette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordPalette
+//
+//  Created by 박주성 on 5/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
