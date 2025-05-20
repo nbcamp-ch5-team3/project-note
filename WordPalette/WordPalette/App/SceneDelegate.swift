1872ba22571211e7c745fbfe932e3e411f2fad7e
gnoes-ios <park_juseong@naver.com>
2025-05-20 14:54:10 +0900

    chore: 프로젝트 초기 세팅
    - CodeBase
    - Rxswift
    - SnapKit
    - gitignore

---
+++ WordPalette/WordPalette/App/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  WordPalette
+//
+//  Created by 박주성 on 5/20/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = HomeViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
--- WordPalette/WordPalette/Presentation/Home/HomeViewController.swift
+++ WordPalette/WordPalette/Presentation/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  WordPalette
 //
 //  Created by 박주성 on 5/20/25.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
