1f6b8a060bd646f43397c9c8604e0da8357ad69d
gnoes-ios <park_juseong@naver.com>
2025-05-21 15:56:40 +0900

    feat: StudyHistory, UserData 엔티티 생성

---
+++ WordPalette/WordPalette/Domain/Entity/StudyHistory.swift
@@ -0,0 +1,13 @@
+//
+//  StudyHistory.swift
+//  WordPalette
+//
+//  Created by 박주성 on 5/21/25.
+//
+
+import Foundation
+
+struct StudyHistory {
+    let id: UUID
+    let solvedAt: Date
+}
---
+++ WordPalette/WordPalette/Domain/Entity/UserData.swift
@@ -0,0 +1,11 @@
+//
+//  UserData.swift
+//  WordPalette
+//
+//  Created by 박주성 on 5/21/25.
+//
+
+struct UserData {
+    let score: Int
+    let studyHistorys: [StudyHistory]
+}
