58ce8c6b9d1fc2686afe5b64b1b9c09fb7e7af69
Sn8Ch0 <whtjsdn98@gmail.com>
2025-05-27 11:34:23 +0900

    feat: 레벨별 버튼 색 수정

--- WordPalette/WordPalette/Presentation/Components/LevelButtonView.swift
+++ WordPalette/WordPalette/Presentation/Components/LevelButtonView.swift
@@ -51,9 +51,22 @@ final class LevelButtonView: UIView {
     func updateButtonSelection(selected: Level) {
         for (index, button) in levelButtons.enumerated() {
             let level = levels[index]
-            button.backgroundColor = (level == selected) ? .customMango : .systemGray5
+
+            if level == selected {
+                switch level {
+                case .beginner:
+                    button.backgroundColor = .customBanana
+                case .intermediate:
+                    button.backgroundColor = .customOrange
+                case .advanced:
+                    button.backgroundColor = .customStrawBerry
+                }
+            } else {
+                button.backgroundColor = .systemGray5
+            }
         }
     }
+
 
     // 버튼이 눌렸음을 Rx로 나타내는 메서드
     func bindButtonTapped() -> Observable<Level> {
