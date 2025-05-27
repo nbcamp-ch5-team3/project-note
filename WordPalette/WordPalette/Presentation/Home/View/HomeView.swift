980bfb1d0bab7a3dc14460b413b3eac37aa9b3fe
Sn8Ch0 <whtjsdn98@gmail.com>
2025-05-27 15:11:55 +0900

    feat: 홈화면 UI 수정

--- WordPalette/WordPalette/Presentation/Home/View/HomeView.swift
+++ WordPalette/WordPalette/Presentation/Home/View/HomeView.swift
@@ -6,38 +6,38 @@ final class HomeView: UIView {
     /// Level 배열
     let levels: [Level] = [.beginner, .intermediate, .advanced]
 
-    /// 단어 추가 label
-    private let addWordLabel = UILabel().then {
-        $0.text = "단어 추가"
-        $0.font = .systemFont(ofSize: 32, weight: .heavy)
-        $0.textColor = .black
-    }
+//    /// 단어 추가 label
+//    private let addWordLabel = UILabel().then {
+//        $0.text = "단어 추가"
+//        $0.font = .systemFont(ofSize: 32, weight: .heavy)
+//        $0.textColor = .black
+//    }
 
-    /// 레벨별로 단어를 검색해서 추가하는 페이지로 가기 위한 버튼 묶음 stackView
-    private lazy var levelSearchButtonStackView = UIStackView(arrangedSubviews: levelSearchButtons).then {
-        $0.axis = .horizontal
-        $0.spacing = 40
-        $0.distribution = .fillEqually
-    }
+//    /// 레벨별로 단어를 검색해서 추가하는 페이지로 가기 위한 버튼 묶음 stackView
+//    private lazy var levelSearchButtonStackView = UIStackView(arrangedSubviews: levelSearchButtons).then {
+//        $0.axis = .horizontal
+//        $0.spacing = 40
+//        $0.distribution = .fillEqually
+//    }
 
-    /// 레벨별 검색 페이지 버튼 배열 (초급, 중급, 고급)
-    lazy var levelSearchButtons: [UIButton] = levels.map { level in
-        let button = UIButton().then {
-            $0.setTitle(level.rawValue, for: .normal)
-            $0.setTitleColor(.white, for: .normal)
-            $0.titleLabel?.font = .systemFont(ofSize: 26, weight: .heavy)
-            $0.layer.cornerRadius = 12
-            $0.clipsToBounds = true
-            $0.backgroundColor = {
-                switch level {
-                case .beginner: return .customBanana
-                case .intermediate: return .customOrange
-                case .advanced: return .customStrawBerry
-                }
-            }()
-        }
-        return button
-    }
+//    /// 레벨별 검색 페이지 버튼 배열 (초급, 중급, 고급)
+//    lazy var levelSearchButtons: [UIButton] = levels.map { level in
+//        let button = UIButton().then {
+//            $0.setTitle(level.rawValue, for: .normal)
+//            $0.setTitleColor(.white, for: .normal)
+//            $0.titleLabel?.font = .systemFont(ofSize: 26, weight: .heavy)
+//            $0.layer.cornerRadius = 12
+//            $0.clipsToBounds = true
+//            $0.backgroundColor = {
+//                switch level {
+//                case .beginner: return .customBanana
+//                case .intermediate: return .customOrange
+//                case .advanced: return .customStrawBerry
+//                }
+//            }()
+//        }
+//        return button
+//    }
 
     /// 내 단어장 label
     private let myWordLabel = UILabel().then {
@@ -55,13 +55,30 @@ final class HomeView: UIView {
         $0.backgroundColor = .clear
     }
 
+    /// 홈 화면 버튼 묶음 stackView
+    private lazy var homeButtonStackView = UIStackView(arrangedSubviews: [toAddWordButton, deleteAllButton]).then {
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.distribution = .fillEqually
+    }
+
+    /// 단어 추가 페이지로 넘어가는 버튼
+    let toAddWordButton = UIButton().then {
+        $0.setTitle("단어 추가", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        $0.backgroundColor = .customOrange
+        $0.layer.cornerRadius = 10
+        $0.clipsToBounds = true
+    }
+
     /// 전체 삭제 버튼
     let deleteAllButton = UIButton().then {
         $0.setTitle("전체 삭제", for: .normal)
         $0.setTitleColor(.white, for: .normal)
         $0.titleLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
-        $0.backgroundColor = .customOrange
-        $0.layer.cornerRadius = 12
+        $0.backgroundColor = .systemGray
+        $0.layer.cornerRadius = 10
         $0.clipsToBounds = true
     }
 
@@ -80,27 +97,28 @@ final class HomeView: UIView {
         backgroundColor = .white
 
         [
-            addWordLabel,
-            levelSearchButtonStackView,
+//            addWordLabel,
+//            levelSearchButtonStackView,
             myWordLabel,
             levelButtonView,
             myWordTableView,
-            deleteAllButton
+            homeButtonStackView
         ].forEach { addSubview($0) }
 
-        addWordLabel.snp.makeConstraints {
-            $0.top.equalTo(safeAreaLayoutGuide)
-            $0.leading.equalTo(safeAreaLayoutGuide).offset(24)
-        }
-
-        levelSearchButtonStackView.snp.makeConstraints {
-            $0.top.equalTo(addWordLabel.snp.bottom).offset(18)
-            $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(24)
-            $0.height.equalTo(levelSearchButtonStackView.snp.width).dividedBy(4)
-        }
+//        addWordLabel.snp.makeConstraints {
+//            $0.top.equalTo(safeAreaLayoutGuide)
+//            $0.leading.equalTo(safeAreaLayoutGuide).offset(24)
+//        }
+//
+//        levelSearchButtonStackView.snp.makeConstraints {
+//            $0.top.equalTo(addWordLabel.snp.bottom).offset(18)
+//            $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(24)
+//            $0.height.equalTo(levelSearchButtonStackView.snp.width).dividedBy(4)
+//        }
 
         myWordLabel.snp.makeConstraints {
-            $0.top.equalTo(levelSearchButtonStackView.snp.bottom).offset(24)
+            $0.top.equalTo(safeAreaLayoutGuide)
+//            $0.top.equalTo(levelSearchButtonStackView.snp.bottom).offset(24)
             $0.leading.equalTo(safeAreaLayoutGuide).offset(24)
         }
 
@@ -116,10 +134,10 @@ final class HomeView: UIView {
             $0.bottom.equalTo(deleteAllButton.snp.top).offset(-18)
         }
 
-        deleteAllButton.snp.makeConstraints {
+        homeButtonStackView.snp.makeConstraints {
             $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(24)
-            $0.height.equalTo(55)
-            $0.bottom.equalTo(safeAreaLayoutGuide).offset(-18)
+            $0.height.equalTo(54)
+            $0.bottom.equalTo(safeAreaLayoutGuide).offset(-10)
         }
     }
 }
